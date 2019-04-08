91fcfc87063558946ff7dcf833fcca5d265fcd60
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-08 17:04:01 -0400

    Initial setup of index.js

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,5 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const dummyReducer = () => {
+  return { todos: [] }
+}
+
+const store = createStore(dummyReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />,
+  </Provider>
+  document.getElementById('root'));
